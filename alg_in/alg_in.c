5c8570153f54770c04290c9212f85706e45025c7
MyNameIsJhon <rigajordan@gmail.com>
2023-08-05 10:30:59 -0700

    Update

--- alg_in/alg_in.c
+++ alg_in/alg_in.c
@@ -3,5 +3,31 @@
 #include "lem_list.h"
 #include "alg_in.h"
 
+lem_p *lem_start_finder(lem_p *lemp_map)
+{
+    lem_p *curs = NULL;
 
+    if(!lemp_map)
+        return NULL;
 
+    curs = lemp_map;
+
+    while(curs != NULL)
+    {
+        if(curs->startorend = 1)
+            return curs;
+        curs = curs->next;
+    }
+    
+    return curs;
+}
+
+t_list *lem_bestway_finder(lem_p *lemp_map)
+{
+    t_list **finder;
+
+    if(!lemp_map)
+        return NULL;
+    
+
+}
