d0ca19865511f2e127c43643e25dac90309a7f93
MyNameIsJhon <rigajordan@gmail.com>
2023-08-05 10:34:40 -0700

    Update

--- alg_in/alg_in.c
+++ alg_in/alg_in.c
@@ -22,12 +22,37 @@ lem_p *lem_start_finder(lem_p *lemp_map)
     return curs;
 }
 
-t_list *lem_bestway_finder(lem_p *lemp_map)
+lem_p *lem_end_finder(lem_p *lemp_map)
 {
-    t_list **finder;
+    lem_p *curs = NULL;
 
     if(!lemp_map)
         return NULL;
+
+    curs = lemp_map;
+
+    while(curs != NULL)
+    {
+        if(curs->startorend = 2)
+            return curs;
+        curs = curs->next;
+    }
+    
+    return curs;
+}
+
+
+
+t_list *lem_bestway_finder(lem_p *lemp_map)
+{
+    t_list **finder;
+    lem_p *lemp_start = NULL;
+
+    if(!lemp_map)
+        return NULL;
+    if(!(finder = (t_list**) malloc(sizeof(t_list*) * 2)))
+        return NULL;
+    
     
 
 }
